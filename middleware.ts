648f8f164bac5281312c8c9a8433c1b9b6e20343
dk5761 <darshankadam714@gmail.com>
2023-07-27 01:38:40 +0530

    added prisma

---
+++ middleware.ts
@@ -0,0 +1,7 @@
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware({ debug: true });
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
